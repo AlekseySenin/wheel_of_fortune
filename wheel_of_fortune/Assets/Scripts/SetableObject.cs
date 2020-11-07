76cbf4014f1a1d573074b83e9f8296cd9fec5cb7
Aleksey Senin <alekseysenin86@gmail.com>
2020-11-07 08:30:46 +0200

    SetableObject.cs

---
+++ wheel_of_fortune/Assets/Scripts/SetableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SetableObject<T> : MonoBehaviour
+{
+    protected static Action<T> OnSetup;
+    public static void Setup(T val)
+    {
+        OnSetup?.Invoke(val);
+    }
+
+    protected abstract void SetInstance(T val);
+   
+
+    protected virtual void Start()
+    {
+        OnSetup += SetInstance;
+    }
+
+}
